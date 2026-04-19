1e663fedcd7cdfc4db6dd8997c0788a22224a6c5
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2026-04-19 17:48:42 -0600

    Parse expense dates using date.fromisoformat
    Add -t/--date option for add and update commands and parse input
    dates with datetime.date.fromisoformat. Reformat argument declarations
    and improve help text for required fields

--- spend/parser.py
+++ spend/parser.py
@@ -1,36 +1,59 @@
 import argparse
+from datetime import date
 
 
 def build_parser() -> argparse.ArgumentParser:
-    
+
     # Main Parser
     parser = argparse.ArgumentParser(
         description="Spend is a command-line tool for tracking and managing your expenses.",
         prog="spend",
         epilog="A project from @Roadmap.sh",
     )
-    
+
     # Command Subparser
     subparser = parser.add_subparsers(
         title="Commands", dest="command", required=True, help="Available commands"
     )
-    
+
     # Command Subparser - Add
     add_subparser = subparser.add_parser("add", help="Add a new Expense")
     add_subparser.add_argument(
-        "-d", "--description", help="Expense description", type=str, required=True
+        "-d",
+        "--description",
+        help="Expense description *REQUIRED",
+        type=str,
+        required=True,
     )
-    add_subparser.add_argument("-a", "--amount", help="Expense amount", type=float, required=True)
-    
+    add_subparser.add_argument(
+        "-a", "--amount", help="Expense amount *REQUIRED", type=float, required=True
+    )
+    add_subparser.add_argument(
+        "-t", "--date", help="Date of the expense YYYY-mm-dd", type=date.fromisoformat
+    )
+
     # Command Subparser - Update
     update_subparser = subparser.add_parser("update", help="Update an Expense data")
-    update_subparser.add_argument("-i", "--id", help="ID of the expense to udpate *REQUIRED", type=int, required=True)
-    
+    update_subparser.add_argument(
+        "-i",
+        "--id",
+        help="ID of the expense to udpate *REQUIRED",
+        type=int,
+        required=True,
+    )
+
     # Command Subparser - Update - Expense Data Argument Group
-    expense_data_ag = update_subparser.add_argument_group(title="Expense data", description="Information to update on the expense")
-    expense_data_ag.add_argument("-d", "--description", help="New Expense description", type=str)
-    expense_data_ag.add_argument("-a", "--amount", help="New Expense amount", type=float)
-    
-    
+    expense_data_ag = update_subparser.add_argument_group(
+        title="Expense data", description="Information to update on the expense"
+    )
+    expense_data_ag.add_argument(
+        "-d", "--description", help="New Expense description", type=str
+    )
+    expense_data_ag.add_argument(
+        "-a", "--amount", help="New Expense amount", type=float
+    )
+    expense_data_ag.add_argument(
+        "-t", "--date", help="New Date amount", type=date.fromisoformat
+    )
 
     return parser
