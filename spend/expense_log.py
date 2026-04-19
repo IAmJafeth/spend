629e6596b04feae5dc36a0a97d6ba92126d2d482
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2026-04-19 15:47:12 -0600

    Add ExpenseLog dataclass for tracking expenses

---
+++ spend/expense_log.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+from datetime import date as d
+
+from expense import Expense
+
+
+@dataclass
+class ExpenseLog:
+    id_counter: int = field(default=1)
+    expenses: dict[int, Expense] = field(default_factory=dict)
+
+    def add_expense(
+        self,
+        description: str = "Not specified",
+        amount: float = 0.0,
+        date: d | None = None,
+    ) -> Expense:
+        date = date or d.today()
+        expense = Expense(self.id_counter, description, amount, date)
+        self.expenses[expense.id] = expense
+        self.id_counter += 1
+        return expense
