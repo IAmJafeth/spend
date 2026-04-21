49ebe0f1332c773224ca4b98cf223d1db2fe121a
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2026-04-21 10:11:24 -0600

    Fix return type annotation in build_parser

--- spend/parser.py
+++ spend/parser.py
@@ -2,7 +2,7 @@ import argparse
 from datetime import date
 
 
-def build_parser() -> tuple[argparse.ArgumentParser, dict[str: argparse.ArgumentParser]]:
+def build_parser() -> tuple[argparse.ArgumentParser, dict[str, argparse.ArgumentParser]]:
 
     # Main Parser
     parser = argparse.ArgumentParser(
